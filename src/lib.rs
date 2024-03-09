7577a4620e1883902bfdc15fbbd283845dfceb1f
Félix Saparelli <felix@passcod.name>
2024-03-09 15:34:59 +1300

    wip: Write process-wrap traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,9 @@
-//! An extension to [`std::process::Command`] to support process groups on Unix and Windows.
-#![cfg_attr(
-	feature = "with-tokio",
-	doc = "With Tokio, the [`AsyncCommandGroup`] trait extends [`tokio::process::Command`](::tokio::process::Command)."
-)]
-#![doc = "\n"]
-#![cfg_attr(
-	unix,
-	doc = "On Unix, the [`UnixChildExt`] trait additionally provides support for sending signals to processes and process groups (it’s implemented on this crate’s [`GroupChild`],"
-)]
-#![cfg_attr(
-	all(unix, feature = "with-tokio"),
-	doc = "[`AsyncGroupChild`], Tokio’s [`Child`](::tokio::process::Child)"
-)]
-#![cfg_attr(unix, doc = "and std’s [`Child`](std::process::Child)).")]
 #![doc(html_favicon_url = "https://watchexec.github.io/logo:command-group.svg")]
 #![doc(html_logo_url = "https://watchexec.github.io/logo:command-group.svg")]
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
 
-pub mod stdlib;
+// #[cfg(feature = "std")]
+// pub mod std;
 
-#[cfg(unix)]
-mod unix_ext;
-
-#[cfg(feature = "with-tokio")]
+#[cfg(feature = "tokio1")]
 pub mod tokio;
-
-pub mod builder;
-
-#[cfg(windows)]
-pub(crate) mod winres;
-
-#[cfg(unix)]
-#[doc(inline)]
-pub use crate::unix_ext::UnixChildExt;
-#[cfg(unix)]
-#[doc(no_inline)]
-pub use nix::sys::signal::Signal;
-
-#[doc(inline)]
-pub use crate::stdlib::child::GroupChild;
-pub use crate::stdlib::CommandGroup;
-
-#[cfg(feature = "with-tokio")]
-#[doc(inline)]
-pub use crate::tokio::child::AsyncGroupChild;
-#[cfg(feature = "with-tokio")]
-pub use crate::tokio::AsyncCommandGroup;
--- src/tokio.rs
+++ src/tokio.rs
@@ -1,133 +1,64 @@
-//! Implementation of process group extensions for [Tokio](https://tokio.rs)’s
-//! asynchronous [`Command` type](::tokio::process::Command).
+use std::any::TypeId;
 
-use std::{
-	io::Result,
-	process::{ExitStatus, Output},
-};
-
+use indexmap::IndexMap;
 use tokio::process::Command;
+use tracing::debug;
 
-use crate::{builder::CommandGroupBuilder, AsyncGroupChild};
+pub struct TokioCommandWrap {
+	command: Command,
+	wrappers: IndexMap<TypeId, Box<dyn TokioCommandWrapper>>,
+}
 
-#[doc(inline)]
-pub use erased::ErasedChild;
-
-#[cfg(target_family = "windows")]
-mod windows;
-
-#[cfg(target_family = "unix")]
-mod unix;
-
-pub(crate) mod child;
-pub(crate) mod erased;
-
-/// Extensions for [`Command`](::tokio::process::Command) adding support for process groups.
-///
-/// This uses [`async_trait`] for now to provide async methods as a trait.
-#[async_trait::async_trait]
-pub trait AsyncCommandGroup {
-	/// Executes the command as a child process group, returning a handle to it.
-	///
-	/// By default, stdin, stdout and stderr are inherited from the parent.
-	///
-	/// On Windows, this creates a job object instead of a POSIX process group.
-	///
-	/// # Examples
-	///
-	/// Basic usage:
-	///
-	/// ```no_run
-	/// # #[tokio::main]
-	/// # async fn main() {
-	/// use tokio::process::Command;
-	/// use command_group::AsyncCommandGroup;
-	///
-	/// Command::new("ls")
-	///         .group_spawn()
-	///         .expect("ls command failed to start");
-	/// # }
-	/// ```
-	fn group_spawn(&mut self) -> Result<AsyncGroupChild> {
-		self.group().spawn()
+impl TokioCommandWrap {
+	pub fn new(command: Command) -> Self {
+		Self { command, wrappers: IndexMap::new() }
 	}
 
-	/// Converts the implementor into a [`CommandGroupBuilder`](crate::CommandGroupBuilder), which can be used to
-	/// set flags that are not available on the `Command` type.
-	fn group(&mut self) -> crate::builder::CommandGroupBuilder<tokio::process::Command>;
-
-	/// Executes the command as a child process group, waiting for it to finish and
-	/// collecting all of its output.
-	///
-	/// By default, stdout and stderr are captured (and used to provide the
-	/// resulting output). Stdin is not inherited from the parent and any
-	/// attempt by the child process to read from the stdin stream will result
-	/// in the stream immediately closing.
-	///
-	/// On Windows, this creates a job object instead of a POSIX process group.
-	///
-	/// # Examples
-	///
-	/// ```should_panic
-	/// # #[tokio::main]
-	/// # async fn main() {
-	/// use tokio::process::Command;
-	/// use std::io::{self, Write};
-	/// use command_group::AsyncCommandGroup;
-	///
-	/// let output = Command::new("/bin/cat")
-	///                      .arg("file.txt")
-	///                      .group_output()
-	///                      .await
-	///                      .expect("failed to execute process");
-	///
-	/// println!("status: {}", output.status);
-	/// io::stdout().write_all(&output.stdout).unwrap();
-	/// io::stderr().write_all(&output.stderr).unwrap();
-	///
-	/// assert!(output.status.success());
-	/// # }
-	/// ```
-	async fn group_output(&mut self) -> Result<Output> {
-		let child = self.group_spawn()?;
-		child.wait_with_output().await
+	pub fn wrap<W: TokioCommandWrapper + 'static>(&mut self, wrapper: W) {
+		let typeid = TypeId::of::<W>();
+		let mut wrapper = Some(Box::new(wrapper));
+		let extant = self.wrappers.entry(typeid).or_insert_with(|| wrapper.take().unwrap());
+		if let Some(wrapper) = wrapper {
+			extant.extend(wrapper);
+		}
 	}
 
-	/// Executes a command as a child process group, waiting for it to finish and
-	/// collecting its status.
-	///
-	/// By default, stdin, stdout and stderr are inherited from the parent.
-	///
-	/// On Windows, this creates a job object instead of a POSIX process group.
-	///
-	/// # Examples
-	///
-	/// ```should_panic
-	/// # #[tokio::main]
-	/// # async fn main() {
-	/// use tokio::process::Command;
-	/// use command_group::AsyncCommandGroup;
-	///
-	/// let status = Command::new("/bin/cat")
-	///                      .arg("file.txt")
-	///                      .group_status()
-	///                      .await
-	///                      .expect("failed to execute process");
-	///
-	/// println!("process finished with: {}", status);
-	///
-	/// assert!(status.success());
-	/// # }
-	/// ```
-	async fn group_status(&mut self) -> Result<ExitStatus> {
-		let mut child = self.group_spawn()?;
-		child.wait().await
+	pub fn spawn(mut self) -> tokio::io::Result<Box<dyn TokioChildWrapper>> {
+		let mut command = self.command;
+		for (id, wrapper) in &mut self.wrappers {
+			debug!(?id, "pre_spawn");
+			wrapper.pre_spawn(&mut command)?;
+		}
+
+		let mut child = command.spawn()?;
+		for (id, wrapper) in &mut self.wrappers {
+			debug!(?id, "post_spawn");
+			wrapper.post_spawn(&mut child)?;
+		}
+
+		let mut child = Box::new(child) as Box<dyn TokioChildWrapper>;
+		for (id, wrapper) in &mut self.wrappers {
+			debug!(?id, "wrap_child");
+			child = wrapper.wrap_child(child)?;
+		}
+
+		Ok(child)
 	}
 }
 
-#[async_trait::async_trait]
-impl AsyncCommandGroup for Command {
-	fn group<'a>(&'a mut self) -> CommandGroupBuilder<'a, Command> {
-		CommandGroupBuilder::new(self)
+pub trait TokioCommandWrapper: std::fmt::Debug {
+	// it is guaranteed that `other` will be of the same type as `self`
+	fn extend(&mut self, other: Box<dyn TokioCommandWrapper>);
+
+	fn pre_spawn(&mut self, command: &mut Command) -> tokio::io::Result<()>;
+	fn post_spawn(&mut self, child: &mut tokio::process::Child) -> tokio::io::Result<()>;
+
+	fn wrap_child(&mut self, child: Box<dyn TokioChildWrapper>) -> tokio::io::Result<Box<dyn TokioChildWrapper>> {
+		Ok(child)
 	}
+
+	fn wait(&mut self, child: &mut tokio::process::Child) -> tokio::io::Result<std::process::ExitStatus>;
 }
+
+pub trait TokioChildWrapper: std::fmt::Debug {}
+impl TokioChildWrapper for tokio::process::Child {}
