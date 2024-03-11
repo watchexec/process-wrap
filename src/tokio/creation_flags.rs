e6e1670a5015296c987bf22bd1f57df6f3bb31a7
Félix Saparelli <felix@passcod.name>
2024-03-11 16:23:33 +1300

    wip: fmt

--- src/tokio/creation_flags.rs
+++ src/tokio/creation_flags.rs
@@ -3,14 +3,14 @@ use std::io::Result;
 use tokio::process::Command;
 use windows::Win32::System::Threading::PROCESS_CREATION_FLAGS;
 
-use super::{TokioCommandWrapper, TokioCommandWrap};
+use super::{TokioCommandWrap, TokioCommandWrapper};
 
 #[derive(Debug, Clone)]
 pub struct CreationFlags(pub PROCESS_CREATION_FLAGS);
 
 impl TokioCommandWrapper for CreationFlags {
 	fn pre_spawn(&mut self, command: &mut Command, _core: &TokioCommandWrap) -> Result<()> {
-		command.creation_flags(self.0.0);
+		command.creation_flags((self.0).0);
 		Ok(())
 	}
 }
