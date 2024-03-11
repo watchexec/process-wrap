fe62ff22bf24a079569a081d34f7c60e068d6e54
Félix Saparelli <felix@passcod.name>
2024-03-12 05:18:23 +1300

    fix: restore Signal re-export on unix

--- src/lib.rs
+++ src/lib.rs
@@ -194,3 +194,6 @@ pub(crate) enum ChildExitStatus {
 	Running,
 	Exited(::std::process::ExitStatus),
 }
+
+#[cfg(all(unix, any(feature = "std", feature = "tokio1")))]
+pub use nix::sys::signal::Signal;
