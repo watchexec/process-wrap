1bd00e4e7fb973cce3e3c0d7e73b6299e4528c33
Félix Saparelli <felix@passcod.name>
2024-03-11 16:21:56 +1300

    wip: make CreationFlags functional on its own

--- src/tokio/creation_flags.rs
+++ src/tokio/creation_flags.rs
@@ -1,8 +1,16 @@
+use std::io::Result;
+
+use tokio::process::Command;
 use windows::Win32::System::Threading::PROCESS_CREATION_FLAGS;
 
-use super::TokioCommandWrapper;
+use super::{TokioCommandWrapper, TokioCommandWrap};
 
 #[derive(Debug, Clone)]
 pub struct CreationFlags(pub PROCESS_CREATION_FLAGS);
 
-impl TokioCommandWrapper for CreationFlags {}
+impl TokioCommandWrapper for CreationFlags {
+	fn pre_spawn(&mut self, command: &mut Command, _core: &TokioCommandWrap) -> Result<()> {
+		command.creation_flags(self.0.0);
+		Ok(())
+	}
+}
