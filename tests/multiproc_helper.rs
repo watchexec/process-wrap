dff0011f5a4f5c92cda6aa5f57d0024efe325840
Félix Saparelli <felix@passcod.name>
2024-04-16 22:38:02 +1200

    Test multiprocess behaviour

---
+++ tests/multiproc_helper.rs
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S cargo +nightly -Zscript
+
+fn main() {
+	eprintln!("selfid={}", std::process::id());
+
+	let n: u8 = std::env::args().nth(1).map_or(0, |s| s.parse().unwrap());
+	let timeout = std::env::args()
+		.nth(2)
+		.map(|s| std::time::Duration::from_secs(s.parse().unwrap()));
+
+	let children = (0..n)
+		.into_iter()
+		.map(|_| {
+			std::process::Command::new(std::env::current_exe().unwrap())
+				.stdout(std::process::Stdio::null())
+				.spawn()
+				.unwrap()
+		})
+		.collect::<Vec<_>>();
+
+	println!("{}:{}", std::process::id(), children.iter().map(|c| c.id().to_string()).collect::<Vec<_>>().join(","));
+
+	if let Some(timeout) = timeout {
+		std::thread::sleep(timeout);
+	} else {
+		std::thread::park();
+	}
+
+	for mut child in children {
+		child.kill().ok();
+	}
+}
---
+++ tests/tokio_unix/multiproc.rs
@@ -0,0 +1,147 @@
+use super::prelude::*;
+
+#[tokio::test]
+async fn process_group_kill_leader() -> Result<()> {
+	let mut leader = TokioCommandWrap::with_new("tests/multiproc_helper.rs", |command| {
+		command.arg("1").arg("10").stdout(Stdio::piped()).stderr(Stdio::null());
+	})
+	.wrap(KillOnDrop)
+	.wrap(ProcessGroup::leader())
+	.spawn()?;
+	assert!(leader.try_wait()?.is_none(), "leader: pre kill");
+
+	sleep(DIE_TIME).await;
+
+	let stdout = leader.stdout().take().expect("Option.unwrap(): get leader stdout");
+	let mut lines = BufReader::new(stdout).lines();
+	let Some(line) = lines.next_line().await? else {
+		panic!("expected line with child pid");
+	};
+	let Some((parent, child)) = line.split_once(':') else {
+		panic!("expected line with parent and child pids");
+	};
+
+	let parent = parent.parse::<i32>().unwrap();
+	let child = child.parse::<i32>().unwrap();
+
+	assert_eq!(parent, leader.id().unwrap() as _);
+	assert!(pid_alive(parent), "parent process should be alive");
+	assert!(pid_alive(child), "child process should be alive");
+
+	Box::into_pin(leader.kill()).await.unwrap();
+	sleep(DIE_TIME).await;
+	assert!(!pid_alive(parent), "parent process should be dead");
+	assert!(!pid_alive(child), "child process should be dead");
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn process_group_kill_group() -> Result<()> {
+	let mut leader = TokioCommandWrap::with_new("tests/multiproc_helper.rs", |command| {
+		command.arg("1").arg("10").stdout(Stdio::piped()).stderr(Stdio::null());
+	})
+	.wrap(KillOnDrop)
+	.wrap(ProcessGroup::leader())
+	.spawn()?;
+	assert!(leader.try_wait()?.is_none(), "leader: pre kill");
+
+	sleep(DIE_TIME).await;
+
+	let stdout = leader.stdout().take().expect("Option.unwrap(): get leader stdout");
+	let mut lines = BufReader::new(stdout).lines();
+	let Some(line) = lines.next_line().await? else {
+		panic!("expected line with child pid");
+	};
+	let Some((parent, child)) = line.split_once(':') else {
+		panic!("expected line with parent and child pids");
+	};
+
+	let parent = parent.parse::<i32>().unwrap();
+	let child = child.parse::<i32>().unwrap();
+
+	assert_eq!(parent, leader.id().unwrap() as _);
+	assert!(pid_alive(parent), "parent process should be alive");
+	assert!(pid_alive(child), "child process should be alive");
+
+	nix::sys::signal::killpg(nix::unistd::Pid::from_raw(parent), Signal::SIGKILL).unwrap();
+	sleep(DIE_TIME).await;
+	assert!(!pid_alive(child), "child process should be dead");
+	Box::into_pin(leader.wait()).await.unwrap();
+	assert!(!pid_alive(parent), "parent process should be dead");
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn process_session_kill_leader() -> Result<()> {
+	let mut leader = TokioCommandWrap::with_new("tests/multiproc_helper.rs", |command| {
+		command.arg("1").arg("10").stdout(Stdio::piped()).stderr(Stdio::null());
+	})
+	.wrap(KillOnDrop)
+	.wrap(ProcessSession)
+	.spawn()?;
+	assert!(leader.try_wait()?.is_none(), "leader: pre kill");
+
+	sleep(DIE_TIME).await;
+
+	let stdout = leader.stdout().take().expect("Option.unwrap(): get leader stdout");
+	let mut lines = BufReader::new(stdout).lines();
+	let Some(line) = lines.next_line().await? else {
+		panic!("expected line with child pid");
+	};
+	let Some((parent, child)) = line.split_once(':') else {
+		panic!("expected line with parent and child pids");
+	};
+
+	let parent = parent.parse::<i32>().unwrap();
+	let child = child.parse::<i32>().unwrap();
+
+	assert_eq!(parent, leader.id().unwrap() as _);
+	assert!(pid_alive(parent), "parent process should be alive");
+	assert!(pid_alive(child), "child process should be alive");
+
+	Box::into_pin(leader.kill()).await.unwrap();
+	sleep(DIE_TIME).await;
+	assert!(!pid_alive(parent), "parent process should be dead");
+	assert!(!pid_alive(child), "child process should be dead");
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn process_session_kill_group() -> Result<()> {
+	let mut leader = TokioCommandWrap::with_new("tests/multiproc_helper.rs", |command| {
+		command.arg("1").arg("10").stdout(Stdio::piped()).stderr(Stdio::null());
+	})
+	.wrap(KillOnDrop)
+	.wrap(ProcessSession)
+	.spawn()?;
+	assert!(leader.try_wait()?.is_none(), "leader: pre kill");
+
+	sleep(DIE_TIME).await;
+
+	let stdout = leader.stdout().take().expect("Option.unwrap(): get leader stdout");
+	let mut lines = BufReader::new(stdout).lines();
+	let Some(line) = lines.next_line().await? else {
+		panic!("expected line with child pid");
+	};
+	let Some((parent, child)) = line.split_once(':') else {
+		panic!("expected line with parent and child pids");
+	};
+
+	let parent = parent.parse::<i32>().unwrap();
+	let child = child.parse::<i32>().unwrap();
+
+	assert_eq!(parent, leader.id().unwrap() as _);
+	assert!(pid_alive(parent), "parent process should be alive");
+	assert!(pid_alive(child), "child process should be alive");
+
+	nix::sys::signal::killpg(nix::unistd::Pid::from_raw(parent), Signal::SIGKILL).unwrap();
+	sleep(DIE_TIME).await;
+	assert!(!pid_alive(child), "child process should be dead");
+	Box::into_pin(leader.wait()).await.unwrap();
+	assert!(!pid_alive(parent), "parent process should be dead");
+
+	Ok(())
+}
