bbe9eede1b6bb6fb522bcc80ebd21210f42c0882
Félix Saparelli <felix@passcod.name>
2024-04-16 22:38:02 +1200

    test: multiprocess behaviour

--- tests/tokio_unix/mod.rs
+++ tests/tokio_unix/mod.rs
@@ -4,17 +4,29 @@ mod prelude {
 	pub use nix::sys::signal::Signal;
 	pub use process_wrap::tokio::*;
 	pub use tokio::{
-		io::{AsyncReadExt, AsyncWriteExt},
+		io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
 		time::sleep,
 	};
 
 	pub const DIE_TIME: Duration = Duration::from_millis(100);
+
+	#[track_caller]
+	pub fn pid_alive(pid: i32) -> bool {
+		#[inline]
+		#[track_caller]
+		fn inner(pid: i32) -> std::result::Result<bool, remoteprocess::Error> {
+			Ok(!remoteprocess::Process::new(pid)?.threads()?.is_empty())
+		}
+
+		inner(pid).unwrap_or(false)
+	}
 }
 
 mod id_same_as_inner;
 mod inner_read_stdout;
 mod into_inner_write_stdin;
 mod kill_and_try_wait;
+mod multiproc;
 mod signals;
 mod try_wait_after_die;
 mod try_wait_twice_after_sigterm;
